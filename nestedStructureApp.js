b1c502561b83c993fd4613b3889daebf8f6ad95c
Bhavana Ingle <bhavanaingle@microsoft.com>
2024-09-14 15:26:16 +0530

    Added sibling structure

--- nestedStructureApp.js
+++ nestedStructureApp.js
@@ -3,6 +3,7 @@
   /* <div id="parent">
   <div id="child">
     <h1> I'm an H1 Tag</h1>
+    <h2> I'm an H1 Tag</h2>  // h1 h2 sibling
   </div>
 </div> */
 }
@@ -10,11 +11,10 @@
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "I'm an H1 Tag")
-  )
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm an H1 Tag"),
+    React.createElement("h2", {}, "I'm an H2 Tag"),
+  ])
 );
 
 console.log(parent); // Here parent is not h1 element its react element which is nothing but an object.
